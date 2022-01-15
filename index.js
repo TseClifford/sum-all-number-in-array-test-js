66d74deedc7c125800c55bc77cd250ecb0aa3b42
Clifford Tse <TseClifford@gmail.com>
2022-01-15 00:35:57 +0000

    added recursion sum function for nested arrays

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+const sum = require("../sum-from-a-to-b-test-js");
+
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let sum = 0;
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      return sum += sumItems(item);
+    } else {
+      return sum += item;
+    }
+  })
+  return sum;
 }
 
 module.exports = sumItems;
